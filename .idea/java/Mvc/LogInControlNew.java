10a51f0fb4765f5a497a4634c3eca16f7b93ca2e
agil3b3ast <agil3b3ast@hotmail.it>
2015-12-01 17:50:01 +0100

    Johhhhhn

---
+++ .idea/java/Mvc/LogInControlNew.java
@@ -0,0 +1,29 @@
+package Mvc;
+
+/**
+ * Created by Alessandro on 01/12/2015.
+ */
+public class LogInControlNew {
+
+    private String ProcessAuth(LogInViewNew livn){
+            return livn.get_auth();
+    }
+
+    public void LogIn(){
+        LogInViewNew livn = new LogInViewNew();
+        LogInModelNew limn = new LogInModelNew();
+
+        if(limn.CheckAuth(this.ProcessAuth(livn))){
+            System.out.println("Tutto bene");
+            return;
+        }
+        System.out.println("Tutto male");
+    }
+
+    public static void main(String[] args){
+        LogInControlNew licn = new LogInControlNew();
+        licn.LogIn();
+        LogInModelNew aa = new LogInModelNew();
+
+    }
+}
---
+++ .idea/java/Mvc/LogInModelNew.java
@@ -0,0 +1,11 @@
+package Mvc;
+
+/**
+ * Created by Alessandro on 01/12/2015.
+ */
+public class LogInModelNew {
+
+    public boolean CheckAuth(String req){
+        return req == "John";
+    }
+}
